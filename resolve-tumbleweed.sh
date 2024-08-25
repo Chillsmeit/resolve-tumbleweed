f6e57dd6d0254a8a7302f9d88d9524efe18f17e2
Bernardo Melo <chillsmeit@proton.me>
2024-08-25 03:49:28 +0100

    Update resolve-tumbleweed.sh

--- resolve-tumbleweed.sh
+++ resolve-tumbleweed.sh
@@ -1,16 +1,75 @@
 #!/bin/bash
-red='\e[38;5;196m'
-redbold='\e[1;38;5;196m'
-orange='\e[38;5;214m'
-orangebold='\e[1;38;5;214m'
-green='\e[38;5;82m'
-greenbold='\e[1;38;5;82m'
-resetcolor='\e[0m'
+clear
 
-if [[ $EUID -eq 0 ]]; then
-	read -p "$(echo -e "${redbold}Please do not run this script as sudo!${resetcolor}")"
-	exit 1
-fi
+check_root(){
+	if [[ $EUID -eq 0 ]]; then
+		read -p "$(printf "\nPlease do not run this script as sudo!")"
+		exit 1
+	fi
+}
+
+check_dir() {
+    local dir=$1
+    [ -d "$dir" ]
+}
+
+get_term_colors(){
+    case "$TERM" in
+        *256color) return 0;;
+        *) return 1;;
+    esac
+}
+
+set_term_colors(){
+    if get_term_colors; then
+    	
+    	# 256 ANSI Custom Colors
+    	orange='\e[38;5;214m'
+    	
+        # 256 ANSI Regular Colors
+        black='\e[38;5;235m'
+        red='\e[38;5;167m'
+        green='\e[38;5;143m'
+        yellow='\e[38;5;221m'
+        blue='\e[38;5;110m'
+        magenta='\e[38;5;182m'
+        cyan='\e[38;5;79m'
+        lightgray='\e[38;5;251m'
+
+        # 256 ANSI Bold Colors
+        darkgray='\e[38;5;242m'
+        lightred='\e[38;5;167m'
+        lightgreen='\e[38;5;149m'
+        lightyellow='\e[38;5;221m'
+        lightblue='\e[38;5;111m'
+        lightmagenta='\e[38;5;183m'
+        lightcyan='\e[38;5;79m'
+        white='\e[38;5;231m'
+    else
+        # 16 ANSI Regular Colors
+        black='\e[0;30m'
+        red='\e[0;31m'
+        green='\e[0;32m'
+        yellow='\e[0;33m'
+        blue='\e[0;34m'
+        magenta='\e[0;35m'
+        cyan='\e[0;36m'
+        lightgray='\e[0;37m'
+
+        # 16 ANSI Bold Colors
+        darkgray='\e[1;30m'
+        lightred='\e[1;31m'
+        lightgreen='\e[1;32m'
+        lightyellow='\e[1;33m'
+        lightblue='\e[1;34m'
+        lightmagenta='\e[1;35m'
+        lightcyan='\e[1;36m'
+        white='\e[1;37m'
+    fi
+
+    # ANSI Reset Color
+    reset='\e[0m'
+}
 
 kill_resolve(){
 	# Get the PID of the current script
@@ -19,36 +78,28 @@ kill_resolve(){
 	pgrep -f "resolve" | grep -v "$current_pid" | xargs -r kill -9 || true
 	pgrep -f "GUI Thread" | xargs -r kill -9
 }
-find_resolve(){
-	echo -e ""
-	echo -e "${orangebold}Please download DaVinci Resolve from:${resetcolor}"
-	echo -e "https://www.blackmagicdesign.com/products/davinciresolve/studio"
-	echo -e ""
-	echo -e "${redbold}Put the zip file in the Downloads folder!${resetcolor}"
-	read -p "Press Enter to continue only after doing the above..."
-	echo -e ""
-	echo -e ""
 
+find_resolve(){
 	cd ~/Downloads
 	matches=($(ls DaVinci_Resolve_*_Linux.zip 2>/dev/null))
 
 	if [ ${#matches[@]} -eq 0 ]; then
-		echo -e "${redbold}No matching files found.${resetcolor}"
-		exit 1
+		printf "\n${red}No matching files found.${reset}"
+		read -p "$(printf "\n${orange}Please put DaVinci Resolve zip in your Downloads Folder...${reset}")"
+		menu_loop
 	fi
-
-	echo -e "${orangebold}Looking for matches....${resetcolor}\n"
+	printf "\n\n${orange}Found the following matches:${reset}\n"
 	for i in "${!matches[@]}"; do
 		printf "%s) %s\n" "$((i+1))" "${matches[$i]}"
 	done
-
+	
 	while true; do
-		read -p "$(echo -e "${orangebold}\nWhich one do you want to use? ${resetcolor}")" choice
+		read -p "$(printf "\nChoose an option: ")" choice
 		if [[ $choice =~ ^[1-9][0-9]*$ ]] && [ "$choice" -le "${#matches[@]}" ]; then
 		    davinci_ext="${matches[$((choice-1))]}"
 		    break
 		else
-		    echo -e "${redbold}Invalid choice, please try again.${resetcolor}"
+		    printf "\n${red}Invalid choice, please try again.${reset}"
 		fi
 	done
 }
@@ -64,26 +115,20 @@ remove_resolve(){
 	rm -f ~/Desktop/com.blackmagicdesign.resolve.desktop
 }
 
-install_resolve(){
-sudo zypper install libapr1-0 libapr-util1-0 libopencl-clang14 libOpenCL1 libOpenCL1-32bit Mesa-libOpenCL libpango-1_0-0 libpango-1_0-0-32bit libpangomm-1_4-1 libpangomm-2_48-1 libjpeg62 libjpeg62-devel
-davinci_no_ext="${davinci_ext%.zip}"
-mkdir /tmp/resolve-tumbleweed
-unzip "$davinci_ext" -d /tmp/resolve-tumbleweed
-cd /tmp/resolve-tumbleweed
-sudo SKIP_PACKAGE_CHECK=1 ./"$davinci_no_ext.run"
+install_dependencies(){
+	sudo zypper install -y libapr1-0 libapr-util1-0 libopencl-clang14 libOpenCL1 libOpenCL1-32bit \
+	Mesa-libOpenCL libpango-1_0-0 libpango-1_0-0-32bit libpangomm-1_4-1 libpangomm-2_48-1 libjpeg62 \
+	libjpeg62-devel
+}
 
-rm -f Linux_Installation_Instructions.pdf
-rm -f ~/Desktop/com.blackmagicdesign.resolve.desktop
-
-mkdir -p $HOME/.local/share/DaVinciResolve
-
+create_launcher(){
 cat << 'EOF' > $HOME/.local/share/DaVinciResolve/davinci-launcher-chillsmeit.sh
 #!/bin/bash
 
 sleep 2
 
-/usr/bin/pgrep -f "GUI Thread" | /usr/bin/xargs -r /bin/kill -9
-/usr/bin/pgrep -f "resolve" | /usr/bin/xargs -r /bin/kill -9
+pgrep -f "GUI Thread" | xargs -r kill -9
+pgrep -f "resolve" | xargs -r kill -9
 
 # Define a file to store the PID of the current instance
 PIDFILE="/tmp/resolve.pid"
@@ -110,9 +155,9 @@ sleep 2
 # Launch DaVinci Resolve
 /opt/resolve/bin/resolve "$@"
 EOF
+}
 
-chmod +x $HOME/.local/share/DaVinciResolve/davinci-launcher-chillsmeit.sh
-
+create_desktop(){
 sudo bash -c 'cat << EOF > /usr/share/applications/com.blackmagicdesign.resolve.desktop
 [Desktop Entry]
 Version=1.0
@@ -128,50 +173,84 @@ StartupNotify=false
 Name[en_US]=DaVinci Resolve
 StartupWMClass=resolve
 EOF'
-
-chmod +x /usr/share/applications/com.blackmagicdesign.resolve.desktop
-sudo cp /opt/resolve/graphics/DV_Resolve.png /usr/share/icons/hicolor/128x128/apps
-sudo cp /opt/resolve/graphics/DV_Resolve.png /usr/share/icons/hicolor/scalable/apps
-sudo gtk-update-icon-cache /usr/share/icons/hicolor
-
-# Download fedora38 gdk-pixbuf2 package (x86_64)
-# https://koji.fedoraproject.org/koji/buildinfo?buildID=2115750
-wget https://kojipkgs.fedoraproject.org//packages/gdk-pixbuf2/2.42.10/2.fc38/x86_64/gdk-pixbuf2-2.42.10-2.fc38.x86_64.rpm
-rpm2cpio ./gdk-pixbuf2-2.42.10-2.fc38.x86_64.rpm | cpio -idmv
-cd usr/lib64/
-sudo cp -vr * /opt/resolve/libs/
-sudo cp -va /lib64/libglib-2.0.* /opt/resolve/libs/
-sudo rm -rf /tmp/resolve-chillsmeit
-
-echo -e ""
-echo -e ""
-read -p "$(echo -e "${greenbold}Installed! Open DaVinci Resolve from the AppMenu")"
-echo -e ""
-echo -e ""
 }
 
-show_help() {
-	echo -e "Usage: $0 <command>"
-	echo -e "Available commands:"
-	echo -e "   install       Will detect and install DaVinci_Resolve.zip in your ~/Downloads and remove any previous installations"
-	echo -e "   uninstall     Will uninstall DaVinciResolve and anything related to this script"
+install_resolve(){
+	davinci_no_ext="${davinci_ext%.zip}"
+	mkdir -p /tmp/resolve-tumbleweed
+	unzip "$davinci_ext" -d /tmp/resolve-tumbleweed
+	cd /tmp/resolve-tumbleweed
+	sudo SKIP_PACKAGE_CHECK=1 ./"$davinci_no_ext.run" -y
+
+	rm -f Linux_Installation_Instructions.pdf
+	rm -f ~/Desktop/com.blackmagicdesign.resolve.desktop
+
+	mkdir -p $HOME/.local/share/DaVinciResolve
+
+	create_launcher
+	chmod +x $HOME/.local/share/DaVinciResolve/davinci-launcher-chillsmeit.sh
+
+	create_desktop
+	chmod +x /usr/share/applications/com.blackmagicdesign.resolve.desktop
+
+	sudo cp /opt/resolve/graphics/DV_Resolve.png /usr/share/icons/hicolor/128x128/apps
+	sudo cp /opt/resolve/graphics/DV_Resolve.png /usr/share/icons/hicolor/scalable/apps
+	sudo gtk-update-icon-cache /usr/share/icons/hicolor
+
+	# Download fedora38 gdk-pixbuf2 package (x86_64)
+	# https://koji.fedoraproject.org/koji/buildinfo?buildID=2115750
+	wget https://kojipkgs.fedoraproject.org//packages/gdk-pixbuf2/2.42.10/2.fc38/x86_64/gdk-pixbuf2-2.42.10-2.fc38.x86_64.rpm
+	rpm2cpio ./gdk-pixbuf2-2.42.10-2.fc38.x86_64.rpm | cpio -idmv
+	cd usr/lib64/
+	sudo cp -vr * /opt/resolve/libs/
+	sudo cp -va /lib64/libglib-2.0.* /opt/resolve/libs/
+	sudo rm -rf /tmp/resolve-chillsmeit
+
+	read -p "$(printf "\n\n${green}Installed!${reset}\n\n")"
 }
 
-case "$1" in
-	install)
-		find_resolve
-		kill_resolve
-		remove_resolve
-		install_resolve
-		;;
-	uninstall)
-		kill_resolve
-		remove_resolve
-		;;
-	*)
-		show_help
-		;;
-esac
+menu_loop(){
+	while true; do
+		clear
+		printf "${orange}     ██████████████████████████████████████████████\n"
+		printf "${orange}     █▓▓▒▒░░ Chillsmeit DaVinci Resolve Fix ░░▒▒▓▓█\n"
+		printf "${orange}|██████████████████████████████████████████████████████|\n"
+		printf "${orange}|█▓▓${white}                                                ${orange}▓▓█|\n"
+		printf "${orange}|█▓▓${white}        1: Install DaVinci Resolve              ${orange}▓▓█|\n"
+		printf "${orange}|█▓▓${white}        2: Uninstall DaVinci Resolve            ${orange}▓▓█|\n"
+		printf "${orange}|█▓▓${white}        0: Exit                                 ${orange}▓▓█|\n"
+		printf "${orange}|█▓▓${white}                                                ${orange}▓▓█|\n"
+		printf "${orange}|██████████████████████████████████████████████████████|${reset}\n\n"
 
-# If there's still issues launch DaVinci with
-# LD_PRELOAD='/usr/lib64/libglib-2.0.so.0 /usr/lib64/libgio-2.0.so.0 /usr/lib64/libgmodule-2.0.so.0' /opt/resolve/bin/resolve
+		read -p "Choose an option: " menuoption
+
+		case "$menuoption" in
+			1)
+				find_resolve
+				kill_resolve
+				remove_resolve
+				install_dependencies
+				install_resolve
+				;;
+			2)
+				kill_resolve
+				remove_resolve
+				;;
+			0)
+				printf "Exiting\n"
+				exit 0
+				;;
+			*)
+				;;
+		esac
+	done
+}
+
+# Checks if Script is being run as root
+check_root
+
+# Set terminal to either 16 ANSI or 256 ANSI
+set_term_colors
+
+# Main Menu loop
+menu_loop
